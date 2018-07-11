0282231e709695acdbd30f9aaa1172163d82f971
Tao <werewaswas@live.com>
2018-07-11 23:43:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,15 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    new(id: id, name: name, breed: breed)
+  end
+    
   def self.find_by_name(name)
+    
   end
 end
         
