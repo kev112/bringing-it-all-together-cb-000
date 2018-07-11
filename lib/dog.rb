1bd447ac0dc3e875f0ca482bc51437f2d09a1af0
Tao <werewaswas@live.com>
2018-07-11 23:26:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,6 +13,7 @@ class Dog
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, name, breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
   
   def self.create(name:,breed:)
