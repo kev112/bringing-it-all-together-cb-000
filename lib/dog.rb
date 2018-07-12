e50885a651bcc2ab716f1de6bde2ec8783987cb8
Tao <werewaswas@live.com>
2018-07-12 00:50:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,5 +93,9 @@ class Dog
     sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
+  
+  def drop_table
+    
+  end
 end
         
